8ddc0db8f188ed7d7aacfc840c50b9b651dc5411
Jordy-Dev <jordy.16182@gmail.com>
2025-04-25 09:05:05 -0500

    Inicio del portafolio

---
+++ script.js
@@ -0,0 +1,10 @@
+// Smooth scroll behavior (if needed for older browsers)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth"
+      });
+    });
+  });
+  
